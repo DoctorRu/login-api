12ec340d1bdd7021f007dea4679bf00075ee7914
DoctorRu <vjblan@gmail.com>
2018-04-14 20:52:14 -0300

    curl eg

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,9 +1,12 @@
 class AuthenticationController < ApplicationController
     skip_before_action :authenticate_request
-    
+
     def authenticate
-        command = AuthenticateUser.call(params[:email], params[:password])
+
+        # curl -H "Content-Type: application/json" -X POST -d '{"email":"example@mail.com","password":"123123123"}' http://localhost:4000/authenticate
         
+        command = AuthenticateUser.call(params[:email], params[:password])
+
         if command.success?
             render json: { auth_token: command.result }
         else
